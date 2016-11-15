ed278f3d9e85c28c9ebdff0e88b2974446669245
Student 8 <Student 8@10.8.33.152>
2016-11-15 13:14:09 -0500

    template for board will work on at home.

--- MedusaSlayer101/src/caveExplorer/VictorAndGabriel/VictorMinesweeperInterpreter.java
+++ MedusaSlayer101/src/caveExplorer/VictorAndGabriel/VictorMinesweeperInterpreter.java
@@ -1,7 +1,24 @@
 package caveExplorer.VictorAndGabriel;
 
 public class VictorMinesweeperInterpreter {
+/** 
+(0,board[row].length - 1)
+row - 1 >= 0
+col + 1 <= board[row].length 
 
+(0,0)
+row - 1 >= 0
+col - 1 >= 0
+
+(0, board.length - 1)
+row + 1 <= board.length
+col - 1 >= 0
+
+(board.length - 1, board[row].length - 1)
+row + 1 <= board.length
+col + 1 <= board[row].length
+
+ */
 	public VictorMinesweeperInterpreter() {
 		
 	}
