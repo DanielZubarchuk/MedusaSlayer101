9968bed1604f0c5f3f2a20eb3baa4ee9f6c4f063
Daniel <dzubarchuk9866@bths.edu>
2016-11-17 00:32:42 -0500

    working.

--- MedusaSlayer101/src/caveExplorer/DanielAndJoyce/DanielBattleCreate.java
+++ MedusaSlayer101/src/caveExplorer/DanielAndJoyce/DanielBattleCreate.java
@@ -1,9 +1,49 @@
 package caveExplorer.DanielAndJoyce;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class DanielBattleCreate {
+	
+	public static Scanner in = new Scanner(System.in);
+	static String[][] userSide;
 
 	public DanielBattleCreate() {
-		// TODO Auto-generated constructor stub
+		int[][] field = new int[5][5];
+		
 	}
+	
+	public static void backField(int[][] field){
+		for(int r = 0 ; r < 5 ; r++ )
+			for(int c = 0 ; c < 5 ; c++)
+				field[r][c] = -1;
+	}
+	
+	public static void makeField(int[][] field){
+        System.out.println("\t1 \t2 \t3 \t4 \t5");
+        System.out.println();
+        
+        for(int row = 0 ; row < 5 ; row++ ){
+            System.out.print((row+1)+"");
+            for(int col = 0 ; col < 5 ; col++ ){
+                if(field[row][col] == -1){
+                    System.out.print("\t"+"~");
+                }else{
+                	if(field[row][col] == 0){
+                		System.out.print("\t"+"*");
+                	}else{
+                		if(field[row][col] == 1){
+                			System.out.print("\t"+"X");
+                		}
+                    
+                	}
+                
+                }
+                System.out.println();
+            }
+        }
+	} 
+	
+	public
 
 }
