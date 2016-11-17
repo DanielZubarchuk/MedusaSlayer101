15e3a9c6d8574af1cd4550f8bac3c9a1ff17e65e
truiko <hotfire619@gmail.com>
2016-11-17 17:53:05 -0500

    Update VictorMinesweeperInterpreter.java

--- MedusaSlayer101/src/caveExplorer/VictorAndGabriel/VictorMinesweeperInterpreter.java
+++ MedusaSlayer101/src/caveExplorer/VictorAndGabriel/VictorMinesweeperInterpreter.java
@@ -6,9 +6,35 @@ public class VictorMinesweeperInterpreter{
 		
 	}
 	
-	public static void initiateGame(int row, int col, String[][] board){
-		board[row][col] = "";
+	public static void interpretInput(int row, int col, String[][] board){
+		board[row][col] = "";	// first click is always blank
 	//	GabrielMinesweeperBoard.createBoard(row, col);
+		int startrow = -1;
+		int startcol = -1;
+		while(startrow != row && startcol != col){
+			if(row - 1 >= 0 && col + 1 <= board[row].length && row + 1 <= board.length && col - 1 >= 0){
+				if(board[row][col - 1] == ""){
+					// change playerboard[row][col - 1] to ""
+					col--;
+				}else{
+					if(board[row][col + 1] == ""){
+						col++;
+					}else{
+						if(board[row - 1][col] == ""){
+							row--;
+						}else{
+							if(board[row + 1][col] == ""){
+								row++;
+							}else{
+								// end of turn and reveal the board 
+								startrow = row;
+								startcol = col;
+							}
+						}
+					}
+				}
+			}	
+		}
 	}
 
 	public static boolean win() {
