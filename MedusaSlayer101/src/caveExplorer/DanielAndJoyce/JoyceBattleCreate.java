78651a9bc3f77bab546519410eb61f6a13b40b7c
sidings <jrna.fng@gmail.com>
2016-11-18 07:51:40 -0500

    heyo

--- MedusaSlayer101/src/caveExplorer/DanielAndJoyce/JoyceBattleCreate.java
+++ MedusaSlayer101/src/caveExplorer/DanielAndJoyce/JoyceBattleCreate.java
@@ -2,21 +2,63 @@ package caveExplorer.DanielAndJoyce;
 
 import java.util.Scanner;
 
+import caveExplorer.DanielAndJoyce.*;
+
 public class JoyceBattleCreate {
 	
 	Scanner ships = new Scanner(System.in);
+	static int FIELDSIZE = 7;
+	static int SHIPNUM = 4;
+	static int CLEAN = -1;
+	static int MISS = 0;
+	static int HIT = 1;
 	
 	public JoyceBattleCreate(){
-		int[][] playerField = new int[5][5];
-		int[][]
+		
+		int[][] pField = new int[FIELDSIZE][FIELDSIZE];
+		int[][] pShips = new int[FIELDSIZE][FIELDSIZE];
+		makeField(pField);
+		initializeField(pField);
 	}
 	
+	public static void initializeField(int[][] field){
+		for(int r = 0 ; r < FIELDSIZE ; r++ ){
+			for(int c = 0 ; c < FIELDSIZE ; c++){
+				field[r][c] = CLEAN;				
+			}
+		}
+	}
+	
+	public static void makeField(int[][] field){
+        System.out.println("\t1 \t2 \t3 \t4 \t5");
+        System.out.println();
+        
+        for(int row = 0 ; row < FIELDSIZE ; row++){
+            System.out.print((row+1)+"");
+            for(int col = 0 ; col < FIELDSIZE ; col++){
+                if(field[row][col] == CLEAN){
+                    System.out.print("\t"+"~");
+                }else{
+                	if(field[row][col] == MISS){
+                		System.out.print("\t"+"*");
+                	}else{
+                		if(field[row][col] == HIT){
+                			System.out.print("\t"+"X");
+                		}
+                    
+                	}
+                
+                }   
+            } 
+            System.out.println("\n");
+        }
+	} 
+	
 	public void userField() {
-		int numShips = 5;
-		for(int i = 0; i<numShips; i++){
-			System.out.println("You have " + numShips + " left. Where would you like your next ship to be placed?");
+		for(int i = 0; i<SHIPNUM; i++){
+			System.out.println("You have " + SHIPNUM + " left. Where would you like your next ship to be placed?");
 			//put on field
-			numShips--;
+			SHIPNUM--;
 		}	
 	}
 	public void createField(){
