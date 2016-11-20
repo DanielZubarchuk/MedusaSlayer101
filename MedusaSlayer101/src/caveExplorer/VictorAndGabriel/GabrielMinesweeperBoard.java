f41a8744fdc812ab3176ce841bef3a87fda4b182
User <User@Acer>
2016-11-19 23:00:16 -0500

    createBoard

--- MedusaSlayer101/src/caveExplorer/VictorAndGabriel/GabrielMinesweeperBoard.java
+++ MedusaSlayer101/src/caveExplorer/VictorAndGabriel/GabrielMinesweeperBoard.java
@@ -4,15 +4,18 @@ import java.util.Random;
 
 public class GabrielMinesweeperBoard {
 
-	private int[][] mineBoard;
-    private char[][] gameBoard;
+	public int[][] mineBoard;
     Random random = new Random();
     
     public GabrielMinesweeperBoard (){
-        mineBoard = new int[10][10];
-        gameBoard = new char[10][10];
-        initiateBoard();
-        randomlyPlaceMines();
+       
+    }
+    
+    public void createBoard(){
+    	 mineBoard = new int[10][10];
+         initiateBoard();
+         randomlyPlaceMines();
+         placeNumbers();
     }
    
     public void initiateBoard(){
@@ -27,7 +30,8 @@ public class GabrielMinesweeperBoard {
     	//mines marked with -1
         boolean randomized;
         randomized = true;
-        int row = 0, col = 0;
+        int row = 0;
+        int col = 0;
         for(int i=0 ; i<10 ; i++){
             while(randomized){
                 row = random.nextInt(8) + 1;
@@ -41,6 +45,22 @@ public class GabrielMinesweeperBoard {
             }
             mineBoard[row][col] = -1;
         }
+    }
+    
+    public void placeNumbers(){
+        for(int row=1 ; row < 9 ; row++){
+            for(int col=1 ; col < 9 ; col++){
+                for(int i=-1 ; i<=1 ; i++){
+                	for(int j=-1 ; j<=1 ; j++){
+                		if(mineBoard[row][col] != -1){
+                			if(mineBoard[row+i][col+j] == -1){
+                				mineBoard[row][col]++;
+                            }
+                        }
+                    }
+                }        
+            }
+        }    
     }
    
 }
