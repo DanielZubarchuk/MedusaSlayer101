aab323c4d985c641950632d093115de927eae3c4
Mahin <mkhan6241@bths.edu>
2016-11-20 22:52:18 -0500

    have to fix valid input

--- MedusaSlayer101/src/caveExplorer/MahinAndVeeraj/MahinMemoryTilesUser.java
+++ MedusaSlayer101/src/caveExplorer/MahinAndVeeraj/MahinMemoryTilesUser.java
@@ -39,8 +39,8 @@ public class MahinMemoryTilesUser {
 			System.out.println("Pick a valid row.");
 			rowPick = userChoiceRow.nextInt();
 		}*/
-		rowPick = userChoiceRow.nextInt();//
-		System.out.println("Pick a column");
+		rowPick = userChoiceRow.nextInt();
+		System.out.println("Pick a column");//
 		/*while(colPick <= 0 || rowPick >= 5){
 			System.out.println("Pick a valid column.");
 			colPick = userChoiceCol.nextInt();
